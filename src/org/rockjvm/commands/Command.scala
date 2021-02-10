f0b909ce4dd99d1f5e05500c80f951229306a62c
Kamil Stawicki <kamilstawicki89@gmail.com>
2021-02-10 19:03:32 +0100

    mkdir command

--- src/org/rockjvm/commands/Command.scala
+++ src/org/rockjvm/commands/Command.scala
@@ -7,6 +7,21 @@ trait Command {
 }
 
 object Command {
-  def from(input: String): Command =
-    new UnknownCommand
+  val MKDIR = "mkdir"
+
+  def emptyCommand: Command = (state: State) => state
+
+  def incompleteCommand(name: String): Command = (state: State) => state.setMessage(s"$name : incomplete command")
+
+  def from(input: String): Command = {
+    val tokens: Array[String] = input.split(" ")
+    if (input.isEmpty || tokens.isEmpty) emptyCommand
+    else {
+      if (MKDIR.equals(tokens(0))) {
+        if (tokens.length < 2) incompleteCommand(MKDIR)
+        else new MkDir(tokens(1))
+      }
+      else new UnknownCommand
+    }
+  }
 }
--- src/org/rockjvm/files/Directory.scala
+++ src/org/rockjvm/files/Directory.scala
@@ -1,10 +1,38 @@
 package org.rockjvm.files
 
+import scala.annotation.tailrec
+
 class Directory(override val parentPath: String,
                 override val name: String,
                 val contents: List[DirEntry])
   extends DirEntry(parentPath, name) {
+  def replaceEntry(entryName: String, newEntry: DirEntry): Directory =
+    new Directory(parentPath, entryName, contents.filter(e => !e.name.equals(entryName)) :+ newEntry)
 
+  def findEntry(entryName: String): DirEntry = {
+    @tailrec
+    def findEntryHelper(name: String, contentList: List[DirEntry]): DirEntry = {
+      if (contentList.isEmpty) null
+      else if (contentList.head.name.equals(name)) contentList.head
+      else findEntryHelper(name, contentList.tail)
+    }
+
+    findEntryHelper(entryName, contents)
+  }
+
+  def addEntry(newEntry: DirEntry): Directory = new Directory(parentPath, name, contents :+ newEntry)
+
+  def hasEntry(name: String): Boolean = findEntry(name) != null
+
+  def getAllFoldersInPath: List[String] =
+    path.substring(1).split(Directory.SEPARATOR).toList.filter(dir => dir.nonEmpty)
+
+  @tailrec
+  final def findDescendant(path: List[String]): Directory =
+    if (path.isEmpty) this
+    else findEntry(path.head).asDirectory.findDescendant(path.tail)
+
+  def asDirectory: Directory = this
 }
 
 object Directory {
